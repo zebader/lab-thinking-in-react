aa4ebbf23431acc1d4c09e33a4533ebb239be566
Jesus Cebader <zebader@hotmail.com>
2019-05-16 15:25:20 +0200

    iteration 4 WIP

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -8,7 +8,8 @@ export default class FilterableProductTable extends Component {
   
     this.state = { 
       data:props.products,
-      allData:props.products
+      allData:props.products,
+     
       }
     }
 
@@ -23,13 +24,39 @@ export default class FilterableProductTable extends Component {
         if (elem.name.includes(searchProduct)){
         result.push(elem)
       
-        } 
+        }
+       
         
       })
-  
+
       this.setState({ data: result }, () => {
         console.log('State',this.state.data)
       })
+  
+    }
+    checkBoxProduct = (checkProduct) =>{
+
+      console.log(checkProduct)
+      const productFilter = [...this.state.data];
+  
+      let result= []; 
+  
+      productFilter.forEach((elem)=>{
+  
+        if (elem.stocked === checkProduct){  
+        result.push(elem)
+        this.setState({ data: result }, () => {
+          console.log('State',this.state.data)
+        })
+        } else {
+          this.setState({ data: this.state.allData }, () => {
+            console.log('State',this.state.data)
+          })
+        }
+        
+      })
+
+
   
     }
 
@@ -38,7 +65,7 @@ export default class FilterableProductTable extends Component {
     return (
       <div>
         <h1>Iron Products</h1>
-        <SearchBar filterProduct={this.filterProduct} />
+        <SearchBar filterProduct={this.filterProduct} checkBoxProduct={this.checkBoxProduct}/>
         <ProductTable products={this.state.data}/>
       </div>
     )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,25 +5,39 @@ export default class SearchBar extends Component {
     super(props);
   
     this.state = { 
-      name:""
+      name:"",
+      checked: true
       }
     
     }
 
     handleChange = (event) =>{ 
+      
       event.preventDefault();
       this.props.filterProduct(event.target.value);
-  
+
       let { name, value } = event.target;
       this.setState( { [name]: value } );
       
     }
+
+    handleChangeCheckBox = (event) =>{     
+      event.preventDefault();
+      console.log(event.target.checked)
+
+      this.setState({checked: event.target.checked},()=>{this.props.checkBoxProduct(this.state.checked)})
+        
+      
+    }
   render() {
+
     return (
       <div className="search">
       
       <form>
           <input name="name" type="text" value={this.state.name} placeholder="Search here" onChange={(e) => this.handleChange(e)} />
+          <input id ="stocked" name="stocked" type="checkbox" placeholder="Search here" onChange={(e) => this.handleChangeCheckBox(e)} />
+          <label htmlFor="stocked">Show only products in stock</label>
       </form>
     </div>
     )
